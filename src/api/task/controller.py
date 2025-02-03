aea21935f5e58c0b11d5ca2fc29b1ba8945c155c
jvras58 <jvras@cin.ufpe.br>
2025-02-03 11:13:56 -0300

    ajusta consultas e criação de tarefas para utilizar o modelo de dados atualizado

--- src/api/task/controller.py
+++ src/api/task/controller.py
@@ -1,16 +1,15 @@
-
 from sqlalchemy.orm import Session
 from api.task import schemas
 from models.task import Task, TaskStatus
 
 def get_task(db: Session, task_id: int):
-    return db.query(Task.Task).filter(Task.Task.id == task_id).first()
+    return db.query(Task).filter(Task.id == task_id).first()
 
 def get_tasks(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Task.Task).offset(skip).limit(limit).all()
+    return db.query(Task).offset(skip).limit(limit).all()
 
 def create_task(db: Session, task: schemas.TaskCreate, description: str):
-    db_task = Task.Task(description=description)
+    db_task = Task(description=description)
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
